b454e31fd6d9575b80a987854efb9bfb6eb9d178
gokul-gopi <gokul@codebuddy.co>
2023-02-06 17:09:26 +0530

    fix: added lazy loading and placeholder for images in post

--- src/pageComponents/Home/Post.tsx
+++ src/pageComponents/Home/Post.tsx
@@ -15,7 +15,15 @@ interface IPost {
 const Post = ({ username, image, likes, comments }: IPost) => {
   return (
     <div className="relative">
-      <Image src={image} alt="post" width={500} height={50} />
+      <Image
+        src={image}
+        alt="post"
+        width={500}
+        height={50}
+        loading="lazy"
+        placeholder="blur"
+        blurDataURL="https://via.placeholder.com/500"
+      />
       <div className="to-gray-50-500 absolute inset-0 flex cursor-pointer flex-col justify-between bg-gradient-to-tr from-[rgba(0,0,0,0.4)] p-4 text-white opacity-0 transition-all delay-100 hover:opacity-100">
         <span>{username}</span>
 
